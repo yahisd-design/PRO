378af52225a70dc5784956dccf8c970f6e0caa34
yahisd-design <57921861+yahisd-design@users.noreply.github.com>
2026-01-28 15:39:47 +0100

    Update check-seconde.js

--- netlify/function/check-seconde.js
+++ netlify/function/check-seconde.js
@@ -1,9 +1,15 @@
 export async function handler(event) {
-  const { password } = JSON.parse(event.body || '{}');
+  const data = JSON.parse(event.body || '{}');
 
-  if (password === 'seconde2025') {
-    return { statusCode: 200, body: 'ok' };
+  if (data.password === 'seconde2025') {
+    return {
+      statusCode: 200,
+      body: 'ok'
+    };
   }
 
-  return { statusCode: 401, body: 'Unauthorized' };
+  return {
+    statusCode: 401,
+    body: 'Unauthorized'
+  };
 }
