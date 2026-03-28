b64588d66ef00ba5f486a7b7e440d9bf8dd90a72
yahisd-design <57921861+yahisd-design@users.noreply.github.com>
2026-03-28 14:19:12 +0100

    Update check-ciel2.js

--- functions/check-ciel2.js
+++ functions/check-ciel2.js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
   const { password } = await request.json();
 
-  if (password === "ciel2_2025") {
+  if (password === "ciel2") {
     return new Response("OK", { status: 200 });
   }
   return new Response("Unauthorized", { status: 401 });
